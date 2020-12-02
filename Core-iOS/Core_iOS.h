8c2244d5da4937335c80388f4898dde40ad8f8ae
Tymoshchenko Mykhailo <morbop@gmail.com>
2020-12-02 16:38:56 +0200

    Initial commit

---
+++ Core-iOS/Core_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Core_iOS.h
+//  Core-iOS
+//
+//  Created by Mikhail Timoscenko on 02.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core_iOS.
+FOUNDATION_EXPORT double Core_iOSVersionNumber;
+
+//! Project version string for Core_iOS.
+FOUNDATION_EXPORT const unsigned char Core_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core_iOS/PublicHeader.h>
+
+
